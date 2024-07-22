33e9af2535244b17a63aeccdac89e90f4b9b153e
张博 <1003326894@qq.com>
2024-07-22 23:05:27 +0800

    Test commit to wordladder2

--- WordLadder2/WordLadder2.cpp
+++ WordLadder2/WordLadder2.cpp
@@ -6,6 +6,6 @@ using namespace std;
 class Solution {
 public:
     vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
-        
+        ;;;
     }
 };
